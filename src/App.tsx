a683d664d06ce6001ffdd226da229391640a42fa
atmik-prodt <atmiktumane72@gmail.com>
2025-11-07 01:33:39 +0530

    first commit - Installations

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+
+const App = () => {
+  return (
+    <MantineProvider>
+      <div>App</div>
+    </MantineProvider>
+  );
+};
+
+export default App;
