04c6269d4afb5189a76cd38a854b573a4b779a44
atmik-prodt <atmiktumane72@gmail.com>
2025-11-11 11:48:51 +0530

    Forgot Password - SendOTP, VerifyOTP, ChangePassword APIs

--- src/Components/ForgotPassword.tsx
+++ src/Components/ForgotPassword.tsx
@@ -1,7 +1,143 @@
-import { Button, Modal, TextInput } from "@mantine/core";
-import { MdOutlineAlternateEmail } from "react-icons/md";
+import {
+  Button,
+  Modal,
+  PasswordInput,
+  PinInput,
+  TextInput,
+} from "@mantine/core";
+import { useInterval } from "@mantine/hooks";
+import { useState } from "react";
+import { MdLockOutline, MdOutlineAlternateEmail } from "react-icons/md";
+import { loginFormValidation } from "../Utils/FormValidation";
+import {
+  errorNotification,
+  successNotification,
+} from "../Utils/NotificationService";
+import {
+  changePasswordAPI,
+  sendOtpAPI,
+  verifyOtpAPI,
+} from "../Services/UserService";
 
 export const ForgotPassword = (props: any) => {
+  // State : to manage email
+  const [email, setEmail] = useState<string>("");
+
+  // State : to manage password
+  const [password, setPassword] = useState<string>("");
+
+  // State : to manage small loader
+  const [smallLoader, setSmallLoader] = useState<boolean>(false);
+
+  // State : to manage sentOTP (To disable button after sending OTP)
+  const [sentOtp, setSentOtp] = useState<boolean>(false);
+
+  // State : to manage Verified OTP (To disable PinInput and buttons after verifying OTP )
+  const [verifiedOtp, setVerifiedOtp] = useState<boolean>(false);
+
+  // State : to manage Password error validation
+  const [passwordError, setPasswordError] = useState<string>("");
+
+  // State : to manage Resent Loader (Resent OTP after 60 seconds)
+  const [resendLoader, setResendLoader] = useState<boolean>(false);
+
+  const [seconds, setSeconds] = useState(60);
+  const interval = useInterval(() => {
+    if (seconds === 0) {
+      setResendLoader(false);
+      setSeconds(60);
+      interval.stop();
+    } else setSeconds((s) => s - 1); // Timer for 60 seconds to enable "Resend OTP" option
+  }, 1000);
+
+  // Handle Send OTP Function
+  const handleSendOTP = async () => {
+    setSmallLoader(true);
+
+    try {
+      // Send OTP API Call
+      const response = await sendOtpAPI(email);
+
+      //   console.log("Send Otp success", response);
+
+      // Disable Send OTP Button
+      setSentOtp(true);
+
+      // Enable Resend Loader
+      setResendLoader(true);
+
+      // Show Success Notification
+      successNotification(response?.message, "Enter OTP to reset Password.");
+
+      // Start ResendOTP countdown
+      interval.start();
+
+      setSmallLoader(false);
+    } catch (error: any) {
+      // Hide Loader
+      setSmallLoader(false);
+
+      // Error Notification
+      errorNotification("Error", "OTP Sending Failed");
+    }
+  };
+
+  // Handle Reset OTP Function
+  const handleResendOtp = () => {
+    // Resent Loader is true, means SendOTP API Call is made already, return
+    if (resendLoader === true) return;
+
+    // Make SendOTP API Call
+    handleSendOTP();
+  };
+
+  // Handle Verify OTP Function
+  const handleVerifyOtp = async (otp: any) => {
+    // console.log(otp);
+
+    try {
+      // Verify OTP API call
+      const response = await verifyOtpAPI(email, otp);
+
+      //   console.log("Verify OTP response : ", response);
+
+      // update State to display Change password
+      setVerifiedOtp(true);
+
+      // Success Notification
+      successNotification(response?.message, "Please change password.");
+    } catch (error: any) {
+      // Error Notification
+      errorNotification("Error", "OTP Verification failed");
+    }
+  };
+
+  // Handle Change Password Function
+  const handleChangePassword = async () => {
+    try {
+      // Reset Password API Call
+      const response = await changePasswordAPI({ email, password });
+
+      // Success Notification
+      successNotification(response?.message, "Login with new password.");
+
+      // Close Modal
+      props.close();
+
+      // Reset all States
+      setEmail("");
+      setPassword("");
+      setSmallLoader(false);
+      setSentOtp(false);
+      setVerifiedOtp(false);
+      setPasswordError("");
+      setResendLoader(false);
+    } catch (error: any) {
+      // Error Notification
+      errorNotification("Error", "Update Password Failed");
+    }
+  };
+
   return (
     <>
       <Modal
@@ -11,30 +147,121 @@ export const ForgotPassword = (props: any) => {
       >
         <div className="flex flex-col gap-5">
           {/* Email Input */}
+          {!sentOtp && (
+            <TextInput
+              size="md"
+              leftSection={<MdOutlineAlternateEmail />}
+              withAsterisk
+              label="Email"
+              placeholder="Your email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              //   error={formError.email}
+              rightSection={
+                <Button
+                  onClick={handleSendOTP}
+                  autoContrast
+                  variant="filled"
+                  className="mr-1"
+                  loading={smallLoader}
+                  disabled={email === "" || sentOtp}
+                  color="cyan.4"
+                  styles={{
+                    label: { color: "white" },
+                  }}
+                >
+                  Send OTP
+                </Button>
+              }
+              rightSectionWidth="xl"
+            />
+          )}
 
-          <TextInput
-            size="sm"
-            leftSection={<MdOutlineAlternateEmail />}
-            withAsterisk
-            label="Email"
-            placeholder="Enter email"
-            name="email"
-            rightSection={
+          {/* PinInput to Enter OTP (Conditional) */}
+          {sentOtp && !verifiedOtp && (
+            <div className="flex flex-col gap-5">
+              {/* PinInput */}
+              <PinInput
+                size="md"
+                length={6}
+                type="number"
+                className="mx-auto"
+                onComplete={handleVerifyOtp}
+              />
+
+              {/* Buttons */}
+              <div className="flex gap-3">
+                {/* Button - Resend OTP button */}
+                <Button
+                  onClick={handleResendOtp}
+                  autoContrast
+                  variant="light"
+                  color="brightSun.4"
+                  loading={smallLoader}
+                  fullWidth
+                >
+                  {resendLoader ? seconds : "Resend OTP"}
+                </Button>
+
+                {/* Button - Change Email button */}
+                <Button
+                  onClick={() => {
+                    setSentOtp(false);
+                    setResendLoader(false);
+                    setSeconds(60);
+                    setVerifiedOtp(false);
+                    interval.stop();
+                  }}
+                  autoContrast
+                  variant="filled"
+                  fullWidth
+                  color="cyan.4"
+                  styles={{
+                    label: { color: "white" },
+                  }}
+                >
+                  Change Email
+                </Button>
+              </div>
+            </div>
+          )}
+
+          {/* Change Password (Conditional) */}
+          {verifiedOtp && (
+            <div className="flex flex-col gap-3">
+              {/* Password Input */}
+              <PasswordInput
+                size="md"
+                withAsterisk
+                label="Password"
+                leftSection={<MdLockOutline />}
+                placeholder="Change Password"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                  setPasswordError(
+                    loginFormValidation("password", e.target.value)
+                  );
+                }}
+                error={passwordError}
+              />
+
+              {/* Button - Change Password Btn */}
               <Button
-                size="xs"
+                onClick={handleChangePassword}
                 autoContrast
                 variant="filled"
-                color="cyan.3"
+                loading={smallLoader}
+                color="cyan.4"
                 styles={{
                   label: { color: "white" },
                 }}
-                className="mr-1"
               >
-                Send OTP
+                Change Password
               </Button>
-            }
-            rightSectionWidth="xl"
-          />
+            </div>
+          )}
         </div>
       </Modal>
     </>
--- src/Services/UserService.ts
+++ src/Services/UserService.ts
@@ -37,4 +37,54 @@ const loginAPI = async (login_data: any) => {
   }
 };
 
-export { signupAPI, loginAPI };
+// POST : Send Otp API
+const sendOtpAPI = async (email: any) => {
+  try {
+    const response = await axios.post(`${apiUrl}/api/v1/user/sendOtp/${email}`);
+
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      throw new Error(error.response.data);
+    } else {
+      console.error("An unexpected error occurred : ", error);
+    }
+  }
+};
+
+// GET : Verify OTP API
+const verifyOtpAPI = async (email: any, otp: any) => {
+  try {
+    const response = await axios.get(
+      `${apiUrl}/api/v1/user/verifyOtp/${email}/${otp}`
+    );
+
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      throw new Error(error.response.data);
+    } else {
+      console.error("An unexpected error occurred : ", error);
+    }
+  }
+};
+
+// POST : Change Password API
+const changePasswordAPI = async (login_data: any) => {
+  try {
+    const response = await axios.post(
+      `${apiUrl}/api/v1/user/changePassword`,
+      login_data
+    );
+
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      throw new Error(error.response.data);
+    } else {
+      console.error("An unexpected error occurred : ", error);
+    }
+  }
+};
+
+export { signupAPI, loginAPI, sendOtpAPI, verifyOtpAPI, changePasswordAPI };
